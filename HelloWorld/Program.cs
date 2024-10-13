7ee1994ee487ec5e8a25899f612e04d399f51889
Piotr Sierant <piotr.sierant96@gmail.com>
2024-10-13 21:06:29 +0200

    Metoda główna

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,48 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// class TestClass
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine(args.Length);
 
-/* Hello World */
-Console.WriteLine("Hello, World!");
+//         // See https://aka.ms/new-console-template for more information
+//         /* Hello World */
+//         Console.WriteLine("Hello, World!");
 
-/* Work with string */
-string firstfriend = "Tom";
-string secondfriend = "Saly";
+//         /* Work with string */
+//         string firstfriend = "Tom";
+//         string secondfriend = "Saly";
 
-Console.WriteLine($"{firstfriend} test {secondfriend}");
-Console.WriteLine($"Length: Tom's length {firstfriend.Length}, Saly's length {secondfriend.Length}");
+//         Console.WriteLine($"{firstfriend} test {secondfriend}");
+//         Console.WriteLine($"Length: Tom's length {firstfriend.Length}, Saly's length {secondfriend.Length}");
 
-/* Trim string */
-string greeting = "    Hello World     ";
-Console.WriteLine(greeting);
+//         /* Trim string */
+//         string greeting = "    Hello World     ";
+//         Console.WriteLine(greeting);
 
-string trimmedGreeting = greeting.Trim();
-Console.WriteLine(trimmedGreeting);
+//         string trimmedGreeting = greeting.Trim();
+//         Console.WriteLine(trimmedGreeting);
 
-string trimmedStartGreetting = greeting.TrimStart();
-Console.WriteLine(trimmedStartGreetting);
+//         string trimmedStartGreetting = greeting.TrimStart();
+//         Console.WriteLine(trimmedStartGreetting);
 
-string trimmedEndGreetting = greeting.TrimEnd();
-Console.WriteLine(trimmedEndGreetting);
+//         string trimmedEndGreetting = greeting.TrimEnd();
+//         Console.WriteLine(trimmedEndGreetting);
 
-string sayHello = "Hello World";
-Console.WriteLine(sayHello);
+//         string sayHello = "Hello World";
+//         Console.WriteLine(sayHello);
 
-/* Replace */
-sayHello = sayHello.Replace("Hello", "Grettings");
-Console.WriteLine(sayHello);
+//         /* Replace */
+//         sayHello = sayHello.Replace("Hello", "Grettings");
+//         Console.WriteLine(sayHello);
 
-/* Upper and Lower */
-Console.WriteLine(sayHello.ToUpper());
-Console.WriteLine(sayHello.ToLower());
+//         /* Upper and Lower */
+//         Console.WriteLine(sayHello.ToUpper());
+//         Console.WriteLine(sayHello.ToLower());
 
-/* Contains */
-string songLyrics = "You say goodbye, and I say hello";
-Console.WriteLine(songLyrics.Contains("goodbye"));
-Console.WriteLine(songLyrics.Contains("greetings"));
+//         /* Contains */
+//         string songLyrics = "You say goodbye, and I say hello";
+//         Console.WriteLine(songLyrics.Contains("goodbye"));
+//         Console.WriteLine(songLyrics.Contains("greetings"));
 
-/* Start and end with */
-Console.WriteLine(songLyrics.StartsWith("You"));
-Console.WriteLine(songLyrics.StartsWith("goodbye"));
+//         /* Start and end with */
+//         Console.WriteLine(songLyrics.StartsWith("You"));
+//         Console.WriteLine(songLyrics.StartsWith("goodbye"));
 
-/* Start and end with */
-Console.WriteLine(songLyrics.EndsWith("hello"));
-Console.WriteLine(songLyrics.EndsWith("goodbye"));
+//         /* Start and end with */
+//         Console.WriteLine(songLyrics.EndsWith("hello"));
+//         Console.WriteLine(songLyrics.EndsWith("goodbye"));
+//     }
+// }
+
+using System.Text;  
+
+StringBuilder builder = new();
+builder.AppendLine("The following arguments are passed:");
+
+foreach (var arg in args)
+{
+    builder.AppendLine($"Argument={arg}");
+}
+Console.WriteLine(builder.ToString());
+
+return 0;
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830982d51c490ba4d602a7d9185144e760181364")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1299bef6d4f34e9c1bbb4178e9447712f7f6f54")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
