72131e243c3a8d1e89a813c5e9d1ee9a2432827c
Piotr Sierant <piotr.sierant96@gmail.com>
2024-10-17 12:07:58 +0200

    FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,11 @@
+﻿for(int i = 1 ; i <= 100; i++) {
+    if(i % 5 == 0 && i % 3 == 0) {
+        Console.WriteLine($"FizzBuzz, {i}");
+    }
+    if(i % 3 == 0) {
+        Console.WriteLine($"Fizz, {i}");
+    }
+    else if(i % 5 == 0) {
+        Console.WriteLine($"Buzz, {i}");
+    }
+}
