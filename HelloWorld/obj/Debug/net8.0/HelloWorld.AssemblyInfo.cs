e81678f70efe763f49e4ae562b14b1af71b85d93
Piotr Sierant <piotr.sierant96@gmail.com>
2024-10-23 12:25:18 +0200

    Challenge project branching looping

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7525bd2b6f525bd85bf80cc15d5c8f07eb8f788")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66431abc44931dba8777406332d43c95fc80c8aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
