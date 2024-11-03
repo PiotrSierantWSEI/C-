5a60fcdc9683e4eab8df70ce494ee956e6930605
Piotr Sierant <piotr.sierant96@gmail.com>
2024-11-03 18:05:37 +0100

    test

---
+++ String-formatting/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string first = "Hello";
+string second = "World";
+string result = string.Format("{0} {1}!", first, second);
+Console.WriteLine(result);
+
