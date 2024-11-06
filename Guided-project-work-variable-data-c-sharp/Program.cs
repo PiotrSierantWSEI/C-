15e10727bc2d96fdfee231ca5f48169db709573e
PiotrSierantWSEI <piotr.sierant@microsoft.wsei.edu.pl>
2024-11-06 13:50:57 +0100

    Projekt z przewodnikiem

---
+++ Guided-project-work-variable-data-c-sharp/Program.cs
@@ -0,0 +1,216 @@
+﻿using System.Globalization;
+
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+// #1 the ourAnimals array will store the following: 
+string animalSpecies = "";
+string animalID = "";
+string animalAge = "";
+string animalPhysicalDescription = "";
+string animalPersonalityDescription = "";
+string animalNickname = "";
+string suggestedDonation = "";
+
+// #2 variables that support data entry
+int maxPets = 8;
+string? readResult;
+string menuSelection = "";
+decimal decimalDonation = 0.00m;
+
+// #3 array used to store runtime data, there is no persisted data
+string[,] ourAnimals = new string[maxPets, 7];
+
+// #4 create sample data ourAnimals array entries
+for (int i = 0; i < maxPets; i++)
+{
+    switch (i)
+    {
+        case 0:
+            animalSpecies = "dog";
+            animalID = "d1";
+            animalAge = "2";
+            animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 45 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+            animalNickname = "lola";
+            suggestedDonation = "85.00";
+            break;
+
+        case 1:
+            animalSpecies = "dog";
+            animalID = "d2";
+            animalAge = "9";
+            animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+            animalNickname = "gus";
+            suggestedDonation = "49.99";
+            break;
+        
+        case 2:
+            animalSpecies = "cat";
+            animalID = "c3";
+            animalAge = "1";
+            animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+            animalPersonalityDescription = "friendly";
+            animalNickname = "snow";
+            suggestedDonation = "40.00";
+            break;
+
+        case 3:
+            animalSpecies = "cat";
+            animalID = "c4";
+            animalAge = "3";
+            animalPhysicalDescription = "Medium sized, long hair, yellow, female, about 10 pounds. Uses litter box.";
+            animalPersonalityDescription = "A people loving cat that likes to sit on your lap.";
+            animalNickname = "Lion";
+            suggestedDonation = "";
+            break;
+        
+        default:
+            animalSpecies = "";
+            animalID = "";
+            animalAge = "";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+            suggestedDonation = "";
+            break;
+
+    }
+
+    ourAnimals[i, 0] = "ID #: " + animalID;
+    ourAnimals[i, 1] = "Species: " + animalSpecies;
+    ourAnimals[i, 2] = "Age: " + animalAge;
+    ourAnimals[i, 3] = "Nickname: " + animalNickname;
+    ourAnimals[i, 4] = "Physical description: " + animalPhysicalDescription;
+    ourAnimals[i, 5] = "Personality: " + animalPersonalityDescription;
+    // ourAnimals[i, 6] = "Suggested donation: " + suggestedDonation;
+
+    if(!decimal.TryParse(suggestedDonation, out decimalDonation))
+    {
+        decimalDonation = 45.00m;
+    }
+    ourAnimals[i, 6] = $"Suggested donation: {decimalDonation:C2}";
+}
+
+// #5 display the top-level menu options
+do
+{
+    // NOTE: the Console.Clear method is throwing an exception in debug sessions
+    Console.Clear();
+
+    Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
+    Console.WriteLine(" 1. List all of our current pet information");
+    Console.WriteLine(" 2. Display all dogs with a specified characteristic");
+    Console.WriteLine();
+    Console.WriteLine("Enter your selection number (or type Exit to exit the program)");
+
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        menuSelection = readResult.ToLower();
+    }
+
+    // use switch-case to process the selected menu option
+    switch (menuSelection)
+    {
+        case "1":
+            // list all pet info
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < 7; j++)
+                    {
+                        Console.WriteLine(ourAnimals[i, j]);
+                    }
+                }
+            }
+            Console.WriteLine("\n\rPress the Enter key to continue");
+            readResult = Console.ReadLine();
+
+            break;
+
+        case "2":
+            // Display all dogs with a specified characteristic
+            string dogCharacteristic = "";
+
+            while(dogCharacteristic == "") {
+                Console.WriteLine("\nEnter one desired dog characteristics to search for");
+                readResult = Console.ReadLine();
+                if (readResult != null)
+                {
+                    dogCharacteristic = readResult.ToLower().Trim();
+                }
+            };
+
+            bool noMatchesDog = true;
+            string dogDescription = "";
+
+            // Console.WriteLine("\nDogs with the characteristic: " + dogCharacteristic);
+
+            for (int i = 0; i < maxPets; i++)
+            {
+                bool dogMatch = true;
+
+                if(ourAnimals[i, 1].Contains("dog")) {
+
+                    if(dogMatch == true) {
+                        // #7 display all dogs with the specified characteristic
+                        dogDescription = ourAnimals[i, 4].ToLower() + ourAnimals[i, 5].ToLower();
+
+                        if(dogDescription.Contains(dogCharacteristic)) {
+                            Console.WriteLine($"Our dog {ourAnimals[i, 3]} is a match!");
+                            Console.WriteLine(dogDescription);
+                            
+                            noMatchesDog = false;
+                        }
+                    }
+                }
+            }
+
+            if (noMatchesDog) {
+                Console.WriteLine("None of our dogs are a match found for: " + dogCharacteristic);
+            }
+
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
+            break;
+
+        default:
+            break;
+    }
+
+} while (menuSelection != "exit");
+
+
+/* 
+
+Projekt Starter dla tego modułu projektu z przewodnikiem zawiera plik Program.cs zawierający następujące funkcje kodu:
+
+kod deklaruje zmienne używane do zbierania i przetwarzania danych zwierząt domowych oraz wyboru elementów menu
+
+kod deklaruje tablicę ourAnimals
+
+kod używa pętli for wokół konstrukcji, aby wypełnić tablicę if--else ifelse ourAnimals przykładowym zestawem danych
+
+w kodzie są wyświetlane następujące opcje menu głównego dla wyboru użytkownika:
+
+Output
+
+Kopiuj
+1. List all of our current pet information
+2. Display all dogs with a specified characteristic
+
+Enter menu item selection or type "Exit" to exit the program
+kod odczytuje wybór elementu menu użytkownika i wyświetla komunikat, który odzwierciedla ich wybór
+
+tylko zaznaczenie "1. Wyświetlanie listy wszystkich naszych bieżących informacji o zwierzętach domowych przy użyciu kodu początkowego
+
+Twoim celem jest zaktualizowanie istniejącego kodu w celu opracowania opisanych wcześniej funkcji aplikacji. Najważniejsze funkcje:
+
+Dodawanie wyszukiwania atrybutów psa
+Uwzględnij sugerowane dane darowizny
+Używasz programu Visual Studio Code jako środowiska programistycznego i testujesz aplikację na każdym etapie procesu programowania.
+
+*/
--- SwitchAndCase/obj/Debug/net8.0/SwitchAndCase.AssemblyInfo.cs
+++ SwitchAndCase/obj/Debug/net8.0/SwitchAndCase.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwitchAndCase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5df65dd9efbf7c704a2377d625863a3e7484831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffcc281ae00c547322723d1e3464aec35ef3baee")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwitchAndCase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwitchAndCase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
