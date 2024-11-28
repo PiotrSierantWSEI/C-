46f572b2acacc94b199a359eaf9fc59e12ff1082
PiotrSierantWSEI <piotr.sierant@microsoft.wsei.edu.pl>
2024-11-28 10:13:55 +0100

    Character Patterns (Act 1)

--- CR-Gwiazdki/Program.cs
+++ CR-Gwiazdki/Program.cs
@@ -6,6 +6,7 @@
     static void Space() => Console.Write(" ");
     static void SpaceLn() => Console.WriteLine(" ");
     static void NewLine() => Console.WriteLine();
+    static void Dot() => Console.Write(".");
 
     public static void Prostokat( int n, int m )
     {
@@ -31,8 +32,57 @@
         NewLine();
     }
 
+    public static void XPattern(int n) {
+        if( n < 3 ) throw new ArgumentException("n must be greater than 3");
+
+        if (n % 2 == 0)
+            n--;
+
+        for( int i = 0; i < n; i++ )
+        {
+            for( int j = 0; j < n; j++ )
+            {
+                if( i == j || i + j == n - 1 )
+                    Star();
+                else
+                    Space();
+            }
+            NewLine();
+        }
+    }
+
+    public static void CPTTRN1(int l, int c) {
+        // l,c < 100
+        for (int i = 0; i < l; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                if (i % 2 == 0) 
+                {
+                    if (j % 2 == 0) {
+                        Star();
+                    } else {
+                        Dot();
+                    }
+                } else {
+                    if (j % 2 == 0) {
+                        Dot();
+                    } else {
+                        Star();
+                    }
+                }
+            }
+            NewLine();
+        }
+    }
+
     public static void Main(string[] args)
     {
-        Prostokat(5, 7);
+        // Prostokat(5, 7);
+        // XPattern(3);
+        // XPattern(7);
+        // XPattern(8);
+        CPTTRN1(3, 3);
+        CPTTRN1(2, 5);
     }
 }
