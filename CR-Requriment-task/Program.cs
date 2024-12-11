14b76156ff74e36569fcdf3cb94104b88b69c0f1
PiotrSierantWSEI <piotr.sierant@microsoft.wsei.edu.pl>
2024-12-11 19:44:27 +0100

    completed requriment task

---
+++ CR-Requriment-task/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CR_Requriment_task {
+    class Program {
+        static void Main() {
+            string[] input = Console.ReadLine().Split(" ");
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+
+            Console.WriteLine(LastDigit(n, m));
+        }
+
+        static int LastDigit(int n, int m) {
+            if (m == 0) return 1;
+            if (n == 0) return 0;
+
+            int[] lastDigits = new int[4] { n % 10, (n * n) % 10, (n * n * n) % 10, (n * n * n * n) % 10 };
+            return lastDigits[(m - 1) % 4];
+        }
+    }
+}
