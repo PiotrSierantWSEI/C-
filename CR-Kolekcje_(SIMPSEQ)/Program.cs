33370d741f4586ac88299d420082462bb4cc6e63
Piotr Sierant <piotr.sierant@microsoft.wsei.edu.pl>
2025-01-19 08:40:05 +0100

    Zadanie 1

---
+++ CR-Kolekcje_(SIMPSEQ)/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SIMPSEQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        public static void Print(int[] a, int[] b)
+        {
+            var A = new SortedSet<int>(a);
+            A.ExceptWith(b);
+            if(A.Count == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+            Console.WriteLine(string.Join(" ", A));
+        }
+    }
+}
+
+
+
